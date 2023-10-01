e7eb26375dbed5a39f15c97c248b375a4380b855
muhammod1 <muhammodholawale@gmail.com>
2023-10-01 16:39:30 +0100

    Implementing UI design for home page

--- src/components/Home/Publish.jsx
+++ src/components/Home/Publish.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Publish() {
   return (
-    <div>Publish</div>
+    <div className='my-16'>
+       <h1 className="font-bold text-2xl text-left  w-[95%]  max-w-[1300px] mx-auto">
+       Recently Published Paper
+      </h1>
+    </div>
   )
 }
