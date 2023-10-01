1d728872f171488dac277729a69b5e8958c1acd1
muhammod1 <muhammodholawale@gmail.com>
2023-10-01 08:55:32 +0100

    Implementing UI design for home page

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { MdArrowRightAlt } from 'react-icons/md'
+import { MdArrowRightAlt } from "react-icons/md";
 
 export default function Hero() {
   return (
     <div className="relative w-[95%] h-[100dvh] max-h-[700px] max-w-[1300px] mx-auto">
       <div className="my-14 top-0 absolute right-0">
+        <div className="absolute -top-14 -left-20 pink h-[55px] w-[55px]" />
         <Image
           src="/images/Hero.svg"
           alt="lang"
@@ -14,6 +15,10 @@ export default function Hero() {
           height={600}
           // priority
         />
+        <div className="absolute -bottom-12 text-[min(2vw,24px)] font-semibold tracking-wider py-12 px-2 text-center text-white right-0 bg-primary ">
+          <p className="text-[min(3vw,42px)] ">720K+</p>
+          Publications
+        </div>
       </div>
       <div className="pt-24 text-black isolate w-[850px] ">
         <p className="text-[min(10vw,70px)] text-balance tracking-wide font-semibold leading-[90px]">
@@ -33,11 +38,11 @@ export default function Hero() {
           </div>
           <div className="mt-5">
             <Link
-                href="#"
-                className="flex item-center gap-2 text-primary font-semibold"
+              href="#"
+              className="flex item-center gap-2 text-primary font-semibold"
             >
-                See all publications
-                <MdArrowRightAlt size={25}/>
+              See all publications
+              <MdArrowRightAlt size={25} />
             </Link>
           </div>
         </div>
