5b7ade8722ab6a5881ce0b0233fd15dbfd7a3354
muhammod1 <muhammodholawale@gmail.com>
2023-10-02 07:55:22 +0100

    Implementing UI design for home page

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="">
     <Nav />
-    <div className='w-full  max-w-[1300px] mx-auto overflow-hidden'>
+    <div className='w-full  max-w-[1300px] mx-auto'>
       <Hero />
       <Explore />
       <Publish />
