313e5248ab9f23e5ef8db1fae28c56e360bcfec0
muhammod1 <muhammodholawale@gmail.com>
2023-10-02 08:10:19 +0100

    Implementing UI design for home page

--- src/components/Home/Blog.jsx
+++ src/components/Home/Blog.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div className="relative w-full">
+      <div className="my-5  w-[95%]  max-w-[1300px] mx-auto">
+      <h1 className="font-bold text-2xl text-left ">
+        bL<span className="text-primary">O</span> p<span className="text-secondary">O</span>sT
+        </h1>
+      </div>
+    </div>
+  );
 }
